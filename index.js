08a518f584f2a1d9d113e71ed63d72636baa591e
Nick <nmalyarchuk07@gmail.com>
2024-10-15 14:00:31 -0400

    .

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const DOMSelectors = {
-  header: document.querySelector("h1"),
+  image: document.querySelector("img"),
   cardHeader: document.querySelector(".card-header"),
-  button: document.querySelector(".sub"),
-  form: document.querySelector("form"),
+  button: document.querySelector("btn"),
+  description: document.querySelector(".Description"),
   container: document.querySelector(".container"),
 };
 
-DOMSelectors.button.addEventListener("click", function (event) {
-  console.groupCollapsed(event.target.parentElement);
-  event.target.parentElement.style.backgroundColor = "red";
-});
+// DOMSelectors.button.addEventListener("click", function (event) {
+//   console.groupCollapsed(event.target.parentElement);
+//   event.target.parentElement.style.backgroundColor = "red";
+// });
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(document.querySelector("input").value);
+  cards();
 });
+
+// document.formRemove()
+// h1.remove()
+
+function cards() {
+  const card = [
+    (header = DOMSelectors.cardHeader),
+    (img = DOMSelectors.image),
+    (desc = DOMSelectors.description),
+  ];
+
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card"><h2 class="card-header">${card.header}</h2><img src="${card.img}"></div>`
+  );
+}
