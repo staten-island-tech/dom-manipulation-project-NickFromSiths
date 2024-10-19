919a8d32397f2dc8e74531b1f9def1b26e5a454f
NickFromSiths <113193039+NickFromSiths@users.noreply.github.com>
2024-10-18 22:34:23 -0400

    how to remove

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const DOMSelectors = {
-  image: document.querySelector("Image"),
-  cardHeader: document.querySelector("Title"),
+  image: document.querySelector("#image"),
+  cardHeader: document.querySelector("#title"),
   button: document.querySelector(".sub"),
-  description: document.querySelector("Description"),
+  description: document.querySelector("#description"),
   container: document.querySelector(".container"),
-  form: document.querySelector("form"),
+
 };
 
-DOMSelectors.form.addEventListener("submit", function (event) {
+DOMSelectors.button.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(document.querySelector(".Title").value);
+  // console.log(DOMSelectors.description.value, DOMSelectors.cardHeader.value, DOMSelectors.image.value);
   // console.log(DOMSelectors.cardHeader);
   cards();
 });
 
 
-
 function cards() {
-  const card = [
-    (header = DOMSelectors.cardHeader),
-    (img = DOMSelectors.image),
-    (desc = DOMSelectors.description),
-  ];
+  const card = {
+    header: DOMSelectors.cardHeader.value,
+    desc: DOMSelectors.description.value,
+    img: DOMSelectors.image.value,
+    
+  };
 
+
+  console.log(card);
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
-    `<div class="card"><h2 class="card-header">${card.header}</h2><img src="${card.img}" alt="" ></div>`
+    `<div class="card"> 
+      <div class="cardcontainer">
+        <h2 class="Title">${card.header}</h2> 
+        <p class="Description">${card.desc}</p> 
+        <img src="${card.img}" alt="${card.header}" class="Image"></img>
+        <button id="remove">Delete</button>
+      </div> 
+    </div>`
   );
+  const DOMselector = {remove: document.querySelector("#remove"),}
+  DOMselector.remove.addEventListener("click", function(event){
+    event.preventDefault();
+    DOMSelectors.container.removeChild();
+  });
 }
 
 
